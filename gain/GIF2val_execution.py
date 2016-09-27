20eaf0986789e6fd25d378452f3479dccd1bf051
Daniel Mingers <d.mingers@fz-juelich.de>
2016-09-27 11:02:47 +0200

    restructured, adapted amp_guess

--- gain/GIF2val_execution.py
+++ gain/GIF2val_execution.py
@@ -7,13 +7,19 @@ srun GIF2val_executaion.py <arraynumber> <maxarraynumber>
 
 TODO: for now, the script only works with current input. To study synaptic
 drive, enable the parameter extraction from the dictionary for poisson drive.
+
+# Recent changes:
+v2:
+Implmented compute_gain2, which corrects for the phase delay in the driving
+generators and uses a phase difference to a fit to the driving signal instead
+of just the phase extracted from the sine fitted to rates.
+
+v3:
+lanning to add the option to give a plot with 25 subplots from single data
+point fits.
 """
 import sys, getopt
 import numpy as np
-# import scipy.optimize as opti
-# import scipy.fftpack as fftpack
-# try: from GIF2val_functions_blau import *
-# except: print('importing my functions did not work!')
 execfile('GIF2val_functions_blau.py')
 
 import nest
@@ -21,10 +27,12 @@ try: nest.set_verbosity('M_ERROR')
 except: print('Changing the nest verbosity did not succeed')
 try: nest.Install("gif2_module")
 except: print ("The GIF2 model had already been installed")
-dt = 0.1 # simulation timestep
+dt = 0.01  # simulation timestep
 freqindex = int(sys.argv[ 1 ])
 maxindex = int(sys.argv[ 2 ])
-nest.SetKernelStatus({"resolution": dt, "print_time": False, "overwrite_files": True})
+nest.SetKernelStatus({"resolution": dt,
+                      "print_time": False,
+                      "overwrite_files": True})
 
 # setting up the frequency array and determining the frequency from the array
 frequencies = np.hstack((np.array([ 0 ]),
@@ -37,10 +45,13 @@ noise_conditions = ['R', 'r']
 
 
 for condition in noise_conditions:
-    nest.SetKernelStatus({"resolution": dt, "print_time": False, "overwrite_files": True})
+    nest.SetKernelStatus({"resolution": dt,
+                          "print_time": False,
+                          "overwrite_files": True})
 
     simparameterdict = import_params_as_dict(filename='jobdict.txt')
-    # contains N, nbins, current/poisson, t_sim, t_rec, t_recstart, simindex, synweight
+    # contains N, nbins, current/poisson, t_sim, t_rec, t_recstart,
+    # simindex, synweight
 
     neuronparamdict = import_params_as_dict(filename='neurondict.txt')
     # contains tau_1, C_m, g_rr=g_1, g, V_m, V_reset, E_L, V_th
@@ -63,46 +74,59 @@ for condition in noise_conditions:
 
     # CREATE DEVICES
     neuron = nest.Create('gif2_psc_exp',
-                         n=int(simparameterdict['N']),
+                         n=int(simparameterdict[ 'N' ]),
                          params=neuronparamdict)
 
     stim_I = nest.Create('ac_generator', params=I_stimdict)
     stim_xi = nest.Create('noise_generator', params=xi_stimdict)
 
+    volty = nest.Create('voltmeter')
+    nest.SetStatus(volty, {"withgid": False,
+                           "withtime": True,
+                           "to_accumulator": True,
+                           "interval": dt})
+
     spikedetector = nest.Create('spike_detector')
     nest.SetStatus(spikedetector, {"withgid": True,
                                    "withtime": True,
-                                   "start": simparameterdict['t_recstart'],
+                                   "start": simparameterdict[ 't_recstart' ],
                                    "to_file": False})
 
 
     # BUILD NETWORK
     # if flag_stim_type == 'poisson':
-    #     nest.Connect(stim_r, neuron, syn_spec={'weight': wfactor * synweight})
+    #    nest.Connect(stim_r, neuron, syn_spec={'weight': wfactor * synweight})
     # elif flag_stim_type == 'current':
     nest.Connect(stim_I, neuron)
     nest.Connect(stim_xi, neuron)
     nest.Connect(neuron, spikedetector)
-
+    nest.Connect(volty, stim_I)
+    nest.Connect(volty, stim_xi)
 
     # SIMULATE
-    nest.Simulate(simparameterdict['t_sim'])
+    nest.Simulate(simparameterdict[ 't_sim' ])
 
 
     # RECORDING
     spike_senders = nest.GetStatus(spikedetector, "events")[ 0 ][ "senders" ]
     spike_times = nest.GetStatus(spikedetector, "events")[ 0 ][ "times" ]
-
+    voltage_trace = nest.GetStatus(volty, "events")[ 0 ][ "V_m" ]
+    voltage_times = nest.GetStatus(volty, "events")[ 0 ][ "times" ]
 
     # EVALUATE
     # Create the istogram to fit with
     hist_bins, hist_heights, hist_binwidth = compute_histogram(
         spike_times, simparameterdict)
+    voltages = voltage_trace[ np.array(hist_bins, dtype=int) ]
 
 
     # Fit and compute gain
-    I_1 = I_stimdict['amplitude']
-    gain, exp_r_0 = compute_gain(hist_bins, hist_heights, hist_binwidth, I_1, f)
+    I_1 = I_stimdict[ 'amplitude' ]
+    multiplotindex = freqindex % (maxindex - (maxindex % 16))/16
+    gain, exp_r_0 = compute_gain2(hist_bins, hist_heights, hist_binwidth, I_1,
+                                  f, dt, voltages, multiplotindex, condition,
+                                  alt_phase=False, printing=True)
+
 
     resultdict = dict(freqindex=freqindex,
                       gain=gain,
@@ -110,5 +134,15 @@ for condition in noise_conditions:
                       neuronparamdict=neuronparamdict,
                       simparameterdict=simparameterdict,
                       condition=condition)
-    nest.ResetKernel()
+
+    # Make plots for both conditions for 25 datapoints
+    if maxindex > 16 and multiplotindex == 0:
+        print('load png files for each condition')
+        print('make current fits')
+        print('print the fits')
+        print('save the files')
+
     write_results(resultdict)
+    nest.ResetKernel()
+
+
