1d56514bfa1fabcc387d477aa0ce920aee55c698
Daniel Mingers <d.mingers@fz-juelich.de>
2017-06-28 17:44:38 +0200

    better display of str plot

--- gif2_validate.py
+++ gif2_validate.py
@@ -74,7 +74,14 @@
 
 import nest
 import numpy as np
-import pylab as pl
+# import pylab as pl
+import matplotlib as mpl
+mpl.use('Agg')
+# import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib import rc 
+rc('mathtext', default='regular')
+# rc('text', usetex=True)
 
 try:
 	nest.Install("gif2_module")
@@ -82,9 +89,9 @@ except:
 	pass
 nest.ResetKernel()
 
-gif_params = dict(tau_1=100.0, C_m=250.0, tau_syn_ex=0.5,
-				  tau_syn_in=0.5, g_rr=25.0, g=25.0,
-				  V_m=0.0, V_th=15.0, t_ref=1.5, V_reset=9.0, E_L=0.0)
+gif_params = dict(tau_1=50.0, C_m=500.0, tau_syn_ex=0.5,
+				  tau_syn_in=0.5, g_rr=50.0, g=10.0, V_m=0.0, 
+                  V_th=15.0, t_ref=1.5, V_reset=9.0, E_L=0.0, I_e=400.0)
 
 # create neuron and multimeter
 n = nest.Create('gif2_psc_exp', params=gif_params)
@@ -93,35 +100,32 @@ m = nest.Create('multimeter', params={'withtime':    True, 'interval': 0.1,
 									  'record_from': [ 'V_m', 'w']})
 
 # Create spike generators and connect
-gex = nest.Create('poisson_generator',
-				  params={'rate': 15 * 60000.0})
-gin = nest.Create('poisson_generator',
-				  params={'rate': 60000.0})
+# gex = nest.Create('poisson_generator',
+# 				  params={'rate': 15 * 60000.0})
+# gin = nest.Create('poisson_generator',
+# 				  params={'rate': 60000.0})
 
-nest.Connect(gex, n, syn_spec={'weight': 0.0125 * 87.8})  # excitatory
-nest.Connect(gin, n, syn_spec={'weight': - 0.0125 * 87.8})  # inhibitory
+# nest.Connect(gex, n, syn_spec={'weight': 0.0125 * 87.8})  # excitatory
+# nest.Connect(gin, n, syn_spec={'weight': - 0.0125 * 87.8})  # inhibitory
 nest.Connect(m, n)
 
 # simulate
-nest.Simulate(100)
+nest.Simulate(50)
+
+# change the input current
+nest.SetStatus(n, {'I_e': 0.0})
+nest.Simulate(300)
 
 # obtain and display data
 events = nest.GetStatus(m)[ 0 ][ 'events' ]
 t = events[ 'times' ];
 
-pl.subplot(211)
-pl.plot(t, events[ 'V_m' ])
-# pl.axis([ 0, 100, -75, -53 ])
-pl.xlim = ([0., 100.])
-pl.ylim = ([-80.0, 20.0])
-pl.ylabel('Membrane potential [mV]')
+fig1 = plt.figure()
+plt.subplot(111)
+plt.plot(t, events[ 'w' ], color='r', label='w')
+plt.plot(t, events[ 'V_m' ], color='b', label='V_m')
+plt.legend()
+plt.ylabel('Membrane potential/2nd membrane variable [mV]')
+plt.xlabel('Time [ms]')
 
-pl.subplot(212)
-pl.plot(t, events[ 'w' ])
-# pl.axis([ 0, 100, 0, 45 ])
-pl.xlim = ([ 0., 100. ])
-pl.ylim = ([ -30.0, 10.0 ])
-pl.xlabel('Time [ms]')
-pl.ylabel('Second variable [nS]')
-
-pl.ion()
+plt.savefig('gif2_str.png')
